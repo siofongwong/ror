e784c5d1a070776ffda7b4559e35eb43a31c3951
Pierre <siofongwong@gmail.com>
2015-04-13 06:34:50 -0700

    first commit

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
+
+    #render plain: params[:article].inspect
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+end
